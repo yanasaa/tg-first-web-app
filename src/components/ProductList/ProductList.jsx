82fdfc5390030ed663bf20ec5f5e010fd32076a6
yanasaa <yanasaa@gmail.com>
2024-06-17 17:37:05 +0300

    add port

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -24,7 +24,7 @@ const ProductList = () => {
       totalPrice: getTotalPrice(addedItems), 
       queryId,
     }
-    fetch('http://localhost:8000', {
+    fetch('http://87.242.106.69:8000', {
       method: 'POST', 
       headers: {
         'Content-Type': 'application/json',
